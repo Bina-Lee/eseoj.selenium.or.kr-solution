26777168b718f0673d0644a6ce6726b847383d99
이진성 <105117441+Bina-Lee@users.noreply.github.com>
2022-10-23 16:56:56 +0900

    1070 1071 1073 1074 1075 1078

--- 1070~1079.c
+++ 1070~1079.c
@@ -120,6 +120,60 @@ int main()
 ////////////////////////////////////////////////////
 
 //1074
+#include <stdio.h>
+#define MAX 100
+ 
+int main() {
+ 
+    unsigned int arr[MAX][2];
+    int n = 0;
+ 
+    scanf("%d", &n);
+ 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+ 
+    // 이 부분에 코드를 작성하세요. ===========================
+ 
+    for (int i = 0; i < n; i++) {
+        int count = 0;
+        int check = 0;
+        switch (arr[i][0]) {
+        case 10:
+            for (int j = 0; j < 30; j++) {
+                if (arr[i][1] == 0)break;
+                if (arr[i][1] % 10 == 0)count++;
+                arr[i][1] = arr[i][1] / 10;
+            }
+            break;
+        case 2:
+            for (int j = 31; j >= 0; j--) {
+                if (arr[i][1] >> j & 0x01)check++;
+                if ((!(arr[i][1] >> j & 0x01)) && check)count++;
+            }
+            break;
+        case 16:
+            for (int j = 7; j >= 0; j--) {
+                if (arr[i][1] >> (j*4) & 0x0F)check++;
+                if (!(arr[i][1] >> (j * 4) & 0x0F) && check)count++;
+            }
+            break;
+        default:
+            break;
+        }
+        printf("%d ", count);
+    }
+ 
+    // ========================================================
+ 
+    return 0;
+}
+
 
 ////////////////////////////////////////////////////
 

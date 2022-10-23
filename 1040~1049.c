d022583946b6f466f6470a1eb2a8ac08705b937d
이진성 <105117441+Bina-Lee@users.noreply.github.com>
2022-10-23 16:53:25 +0900

    1040 1047 1048 1049

--- 1040~1049.c
+++ 1040~1049.c
@@ -29,7 +29,76 @@ int main(){
 ////////////////////////////////////////////////////
 
 //1048
+#include <stdio.h>
+ 
+ 
+// ===============================================
+// 이 부분에 필요한 코드를 작성하시오.
+int sub(int x,int y){
+    return x-y;
+}
+ 
+ 
+// ===============================================
+ 
+ 
+int main() {
+ 
+ 
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", sub(a, b));
+ 
+ 
+    return 0;
+}
+
 
 ////////////////////////////////////////////////////
 
 //1049
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int diffMaxMin(int arr[], int n) {
+// 이 부분에 코드를 작성하시오. ===========================
+ 
+     
+    int min=0,max=0,minvalue=100000,maxvalue=-100000;
+    for(int i=0;i<n;i++){
+        if(arr[i]>maxvalue){
+            maxvalue=arr[i];
+            max=i;
+        }
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]<minvalue){
+            minvalue=arr[i];
+            min=i;
+        }
+    }
+    int res=max-min;
+    if(res<0)res=-res;
+    return res;
+ 
+ 
+ 
+// ========================================================
+}
+ 
+ 
+int main() {
+ 
+    int n;
+    int arr[100];
+ 
+    scanf("%d", &n);
+ 
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+ 
+    printf("%d", diffMaxMin(arr, n));
+    return 0;
+}

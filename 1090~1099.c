a9ad4ef6445330c0e7576aa6b90463a09f9d0050
이진성 <105117441+Bina-Lee@users.noreply.github.com>
2022-10-27 08:37:57 +0900

    1090

--- 1090~1099.c
+++ 1090~1099.c
@@ -1,4 +1,36 @@
-//1090
+//1090 example
+#include <stdio.h> 
+
+
+void SWAP(char* p){
+    char* pnt=p;
+    int cnt1=0;
+    while(*(pnt+cnt1)!=0){
+        if(*(pnt+cnt1)=='a')if(*(pnt+cnt1+1)=='n')if(*(pnt+cnt1+2)=='d'){
+            *(pnt+cnt1)='&';
+            int cnt2=1;
+            while(1){
+                *(pnt+cnt1+cnt2)=*(pnt+cnt1+cnt2+2);
+                if(*(pnt+cnt1+cnt2+2)==0)break;
+                cnt2++;
+            }
+        }
+        cnt1++;
+    }
+} 
+
+
+int main() { 
+     char text[50]; 
+     scanf("%s", text);
+     SWAP(text); 
+     printf("%s", text); 
+     return 0;
+}
+
+////////////////////////////////////////////////////
+
+//1090 without function
 #include<stdio.h>
 int main() {
     char text[50];

83fe46583ba561c35314e71bf716b98565d9fdf6
이진성 <105117441+Bina-Lee@users.noreply.github.com>
2022-10-27 08:39:24 +0900

    1090

--- 1090~1099.c
+++ 1090~1099.c
@@ -27,20 +27,3 @@ int main() {
      printf("%s", text); 
      return 0;
 }
-
-////////////////////////////////////////////////////
-
-//1090 without function
-#include<stdio.h>
-int main() {
-    char text[50];
-    scanf("%s", text);
-    for (int i = 0; i < 50; i++) {
-        if (text[i] == 'a')if (text[i + 1] == 'n')if (text[i + 2] == 'd') {
-            text[i] = '&';
-            for (int j = i + 1; j < 50; j++)text[j] = text[j + 2];
-        }
-        else if (text[i] == 0)break;
-    }
-    printf("%s", text);
-}
